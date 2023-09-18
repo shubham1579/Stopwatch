e88829f756ef2066d59b90a694f7d214c16251c6
Shubham kumar bharti <79shubham88@gmail.com>
2023-09-18 08:40:20 +0530

    added some comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Creating variables for using it
 let msec = 0;
 let sec = 0;
 let min = 0;
 let hour = 0;
 let isRunning = false;
 
+// this function shows the time in the given manner
 function formatTime() {
     if(isRunning){
         msec++;
@@ -24,6 +26,8 @@ function formatTime() {
             sec = 0;
         }
 
+
+        // accessing the class objects for manipulating DOM
         document.querySelector('.ms').innerHTML = (msec < 10) ? "0" + msec : msec;
         document.querySelector('.sec').innerHTML = (sec < 10) ? "0" + sec : sec;
         document.querySelector('.min').innerHTML = (min < 10) ? "0" + min : min;
@@ -33,15 +37,18 @@ function formatTime() {
     }
 }
 
+// this function starts the stopwatch timer
 function startTimer() {
     isRunning = true;
     formatTime();
 }
 
+// this function stops the stopwatch timer
 function stopTimer() {
     isRunning = false;
 }
 
+// this function resets the stopwatch time
 function resetTimer() {
     isRunning = false;
 
@@ -50,12 +57,15 @@ function resetTimer() {
     min = 0;
     hour = 0;
 
+
+    // after reset manipulating the DOM to show the time as 0
     document.querySelector('.ms').innerHTML = "00";
     document.querySelector('.sec').innerHTML = "00";
     document.querySelector('.min').innerHTML = "00";
     document.querySelector('.hour').innerHTML = "00";
 }
 
+// handling the click events on the buttons
 document.getElementById('startbtn').addEventListener('click', startTimer);
 document.getElementById('stopbtn').addEventListener('click', stopTimer);
 document.getElementById('resetbtn').addEventListener('click', resetTimer);
